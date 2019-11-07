08a5d49c65923344d47a22aa5991895356c3eca8
lilyminsky <lminsky@asu.edu>
2019-11-06 20:55:57 -0700

    original mockups

---
+++ Mockups/files/leaderboards/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x,y,z],A,_(B,C,D,E,F,G,H,_(),I,_(J,K,L,M,N,_(O,P,Q,R),S,null,T,M,U,M,V,W,X,null,Y,Z,ba,bb,bc,bd,be,Z),bf,_(),bg,_(),bh,_(bi,[])),bj,_(),bk,_());}; 
+var b="url",c="leaderboards.html",d="generationDate",e=new Date(1573098155371.74),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="AxStWidget",o="AxStYLimit",p="AxStRowHeight",q="AxStYPosition",r="AxStHighest",s="AxStPickedValue",t="AxStXPosition",u="AxStXPositionOriginal",v="AxStOperation",w="AxStColumnWidth",x="AxStXLimit",y="AxStNoItems",z="AxStNoVisibleItems",A="page",B="packageId",C="b0d6ba161fbb4a50b7cb70671bb31112",D="type",E="Axure:Page",F="name",G="Leaderboards",H="notes",I="style",J="baseStyle",K="627587b6038d43cca051c114ac41ad32",L="pageAlignment",M="near",N="fill",O="fillType",P="solid",Q="color",R=0xFFFFFFFF,S="image",T="imageHorizontalAlignment",U="imageVerticalAlignment",V="imageRepeat",W="auto",X="favicon",Y="sketchFactor",Z="0",ba="colorStyle",bb="appliedColor",bc="fontName",bd="Applied Font",be="borderWidth",bf="adaptiveStyles",bg="interactionMap",bh="diagram",bi="objects",bj="masters",bk="objectPaths";
+return _creator();
+})());
