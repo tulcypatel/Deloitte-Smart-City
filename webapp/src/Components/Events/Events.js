09ff25fb00e424efa626c2c12afc738f26cca372
Darren Vasquez <dvasque4@gmail.com>
2020-03-10 23:47:33 -0700

    query works

--- webapp/src/Components/Events/Events.js
+++ webapp/src/Components/Events/Events.js
@@ -1,29 +1,29 @@
 import React, {useState, useEffect} from 'react';
 
+import axios from 'axios';
+
 export default function Events(props) {
 
     const [events, setEvents] = React.useState([]);
     const [loaded, setLoaded] = React.useState(false);
 
     React.useEffect(() => {
-        async function fetchData() {
-            console.log(props.keyword);
-            const res = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?postalCode=85281&radius=200&keyword=` + props.keyword + "&apikey=VydxfkG5lmRqboQK6sEOxluYAIKD9Uxd", {mode: 'no-cors'})
-            res.json()
-            .then(res => setEvents(res), setLoaded(true))
-            .catch(error => console.log(error))
-        }
-        fetchData();
+        axios.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=5otTtHXbkTQSVFXb5wrduaI1GBWm7Xxg')
+        .then(res => {
+            console.log(res)
+            const data = res.data._embedded.events;
+            setEvents(data);
+          })
 
     }, []);
 
-    if(loaded) return (
+    return (
         <div>
-            there are {events.length} events returned
+            test - 
+            {events && events.map(event => (
+                event.name
+            ))}
         </div>
-    );
-    else return (
-        <div>loading</div>
     );
 
 }
