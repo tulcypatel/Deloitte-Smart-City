c93565b66544476e761d6e888bbaf1e63647fe12
Darren Vasquez <dvasque4@gmail.com>
2020-04-14 04:13:18 -0500

    Added Filter to Alerts Page
    Currently only filters by All, Sports, and Food events.

--- webapp/src/Components/Alerts/Alerts.js
+++ webapp/src/Components/Alerts/Alerts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Grid, Typography, Card, CardContent} from '@material-ui/core'
+import { Grid, Typography, Card, CardContent, Button, Menu, MenuItem } from '@material-ui/core'
+import FilterListIcon from '@material-ui/icons/FilterList';
 import { makeStyles } from '@material-ui/core/styles';
 
 import axios from 'axios';
@@ -9,6 +10,9 @@ export default function Alerts(props) {
 
     const [alerts, setAlerts] = React.useState([]);
     //const [loaded, setLoaded] = React.useState(false);
+    const [filter, setFilter] = React.useState("All");
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [filteredAlerts, setFilteredAlerts] = React.useState([]);
 
     const useStyles = makeStyles(theme => ({
         root: {
@@ -21,16 +25,51 @@ export default function Alerts(props) {
         },
         title: {
             fontSize: 14,
-        }
+        },
+        header: {
+            justifyContent: 'space-evenly',
+            textAlign: 'right',
+            padding: '1%',
+        },
       }));
 
     const classes = useStyles();
+
+    const handleClick = (event) => { setAnchorEl(event.currentTarget); }
+
+    const handleMenuItemClick = (event, index) => {
+        setFilter(index);
+        setAnchorEl(null);
+    };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    React.useEffect(() => {
+
+        if(filter != "All") {
+
+            var tempArray = [];
+            for(var index = 0; index < alerts.length; index++) {
+                if(alerts[index].AlertCategory.S == (filter + " Event")) {
+                    tempArray.push(alerts[index]);
+                }
+            }
+            setFilteredAlerts(tempArray);
+
+        } else {
+            setFilteredAlerts(alerts);
+        }
+
+    }, [filter]);
 
     React.useEffect(() => {
         
         axios.get('https://3jkld0ayk3.execute-api.us-west-1.amazonaws.com/AlertAPIBeta2/scan')
         .then(res => {
             setAlerts(res.data.Items);
+            setFilteredAlerts(res.data.Items);
           })
 
     }, []);
@@ -41,13 +80,23 @@ export default function Alerts(props) {
     return (
         <div>
             <div className={classes.root}>
+                <div className={classes.header}>
+                    <Button aria-controls="simple menu" aria-haspopup="true" onClick={handleClick} endIcon={<FilterListIcon/>}>
+                        {filter}
+                    </Button>
+                    <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                        <MenuItem onClick={(event) => handleMenuItemClick(event, "All")}>All</MenuItem>
+                        <MenuItem onClick={(event) => handleMenuItemClick(event, "Sports")}>Sports</MenuItem>
+                        <MenuItem onClick={(event) => handleMenuItemClick(event, "Food")}>Food</MenuItem>
+                    </Menu>
+                </div>
                 <Grid container direction="column-reverse" justify="flex-end" alignItems="stretch" spacing={2}>
-                    {alerts && alerts.map(alert => (
+                    {filteredAlerts && filteredAlerts.map(alert => (
                         <Grid item xs={12} key={alert.AlertID.N}>
                             <Card className={classes.root}>
                                 <CardContent>
                                     <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    {alert.AlertDate.S + " " + alert.AlertTime.S}
+                                        {alert.AlertDate.S + " " + alert.AlertTime.S}
                                     </Typography>
                                     <Typography variant="h5" component="h2">
                                         {alert.AlertTitle.S}
