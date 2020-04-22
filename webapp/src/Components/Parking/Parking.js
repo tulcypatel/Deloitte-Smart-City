856c11d675a3ed7a373086d27b6c4a828ae86da9
Romonte Moore <rlmoore4@asu.edu>
2020-04-21 17:38:59 -0700

    Added info windows to markers.

--- webapp/src/Components/Parking/Parking.js
+++ webapp/src/Components/Parking/Parking.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 
 // import CurrentLocation from './Map';
-const mapStyles = {
-  width: '56%',
-  height: '100%'
-};
-
+const mapStyles = 
+{width: '100%', height: '80%', position: 'relative'};
+// const containerStyle = 
+// {position: 'relative', width: '100%', height: '80%'};
+  // The style is copy from https://snazzymaps.com/style/2/midnight-commander DARK MODE
+  const mapstyles= [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
+  // var styledMapType = new google.maps.StyledMapType(mapstyles)
 // var myLatLng = {lat: 33.360355, lng: -111.801682}
 // var myLng = {lat: 33.359423, lng: -111.021071}
 
@@ -145,10 +147,11 @@ export class Parking extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      showingInfoWindow: false,  //Hides or the shows the infoWindow
-      activeMarker: {},          //Shows the active marker upon click
-      selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker
+    // var add = new this.props.google.maps.zoomControl
+    this.states = {
+      // showingInfoWindow: false,  //Hides or the shows the infoWindow
+      // activeMarker: {},          //Shows the active marker upon click
+      // selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker
       stores: [{lat: 47.49855629475769, lng: -122.14184416996333}, //Does not work for some reason
               {latitude: 33.4255, longitude: -111.9400},
               {latitude: 33.4484, longitude: -112.0740},
@@ -166,57 +169,263 @@ export class Parking extends Component {
     }
   }
 
-    states = {
-      showingInfoWindow: false,  //Hides or the shows the infoWindow
+    state = {
       activeMarker: {},          //Shows the active marker upon click
-      selectedPlace: {}         //Shows the infoWindow to the selected place upon a marker
+      selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker
+      showingInfoWindow: false,  //Hides or the shows the infoWindow
+      // isHeatmapVisible: false
+      // isDarkmodeVisible: false,
+
     };
 
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-        this.setState({
-            showingInfoWindow: false,
-            activeMarker: null
-        });
-    }
-};
+//   onClose = props => {
+//     if (this.states.showingInfoWindow) {
+//         this.setState({
+//             showingInfoWindow: false,
+//             activeMarker: null
+//         });
+//     }
+// };
+
+// onMarkerClick = (props, marker, e) =>
+// this.setState({
+// selectedPlace: props,
+// activeMarker: marker,
+// showingInfoWindow: true
+// });
+
+// onMarkerClick = (props, marker) =>
+//     this.setState({
+//       activeMarker: marker,
+//       selectedPlace: props,
+//       showingInfoWindow: true
+//     });
+
+//   onInfoWindowClose = () =>
+//     this.setState({
+//       activeMarker: null,
+//       showingInfoWindow: false
+//     });
+
+//   onMapClicked = () => {
+//     if (this.states.showingInfoWindow)
+//       this.setState({
+//         activeMarker: null,
+//         showingInfoWindow: false
+//       });
+//   };
+
 
 onMarkerClick = (props, marker, e) =>
-this.setState({
-selectedPlace: props,
-activeMarker: marker,
-showingInfoWindow: true
-});
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+    
+  });
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index}
-      position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => this.onMarkerClick} //console.log("This works")
-     />
-    })
+onClose = props => {
+  if (this.state.showingInfoWindow) {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+    });
   }
+};
+
+  // displayMarkers = () => {
+  //   return this.state.stores.map((store, index) => {
+  //     return <Marker key={index} id={index}
+  //     label={{text: 'hello'}}
+  //     position={{
+  //      lat: store.latitude,
+  //      lng: store.longitude
+  //    }}
+  //    onClick={() => this.onMarkerClick} //console.log("This works")
+  //    />
+  //   })
+  // }
+  // toggleHeatmap() {
+  //   this.setState({isHeatmapVisible: !this.state.isHeatmapVisible})
+  // }
+  toggleDarkmode() {
+    this.setState({isDarkmodeVisible: !this.state.isDarkmodeVisible})
+  }
+  // map = <HeatMap
+  //   gradient={gradient}
+  //   opacity = {3}
+  //   positions = {this.props.policeCall.map(({M, N}) => {
+  //     return {lat: M, lng: N};
+  //   })}
+  //   radius={30}
+  //   />
 
   render() {
     return (
         <Map
           google={this.props.google}
+          // onClick={this.onMarkerClick}
           zoom={10}
           style={mapStyles}
+          // styles={mapstyles}
+          // containerStyle={containerStyle}
           initialCenter={{ lat: 33.360355, lng: -111.801682}}
+          // mapTypeControl = {true} This is where I am trying to edit the control.
+          // mapTypeControlOptions= {{
+          //   style: this.props.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+          //   mapTypeIds:['roadmap', 'darkmode']
+          // }}
         >
-          {this.displayMarkers()}
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Queen Creek town center has 30 spots available'}
+            position={{lat: 33.2486, lng: -111.6346}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Tempe Town Lake has 20 spots available'}
+            position={{lat: 33.4255, lng: -111.9400}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'The Barn and Trailside community park has 80 spots available'}
+            position={{lat:33.284547, lng:-111.698873}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Play Park has 12 spots available'}
+            position={{lat:33.276343, lng:-111.699118}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Arbors Park in Power Ranch has 42 spots available'}
+            position={{lat: 33.275057, lng: -111.708220}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Menlo Park has 36 spots available'}
+            position={{lat: 33.280037, lng: -111.715229}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Veterans Oasis Park has 48 spots available'}
+            position={{lat: 33.235700, lng: -111.767976}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Tumble Weed Park has 57 spots available'}
+            position={{lat: 33.272812, lng: -111.829431}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Freestone Park has 13 spots available'}
+            position={{lat: 33.358641, lng: -111.767833}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Rancho Del Mar Park has 89 spots available'}
+            position={{lat: 33.365546, lng: -111.849588}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Chandler Festival has 137 spots available'}
+            position={{lat: 33.307458, lng: -111.887244}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Chandler Fashion Square has 0 spots available'}
+            position={{lat: 33.301975, lng: -111.897973}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'South Mountain Park and Preserve has 27 spots available'}
+            position={{lat: 33.346158, lng: -112.055558}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Mesquite Groves Aquatic Center has 0 spots available'}
+            position={{lat: 33.220568, lng: -111.763133}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Top Golf has 0 spots available'}
+            position={{lat: 33.321202, lng: -111.741480}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Gilbert Town Square has 200 spots available'}
+            position={{lat: 33.330896, lng: -111.792749}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'San Tan Village Marketplace has 13 spots available'}
+            position={{lat: 33.301295, lng: -111.744110}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'San Tan Mountain Regional Park has 25 spots available'}
+            position={{lat: 33.167336, lng: -111.642830}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Coyote Run Golf Course has 67 spots available'}
+            position={{lat: 33.398581, lng: -111.705644}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'GolfLand Sunsplash has 71 spots available'}
+            position={{lat: 33.387978, lng: -111.837496}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Dobson Ranch Golf Course has 101 spots available'}
+            position={{lat: 33.374174, lng: -111.876285}} 
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Cactus Yards has 83 spots available'}
+            position={{lat: 33.354817, lng: -111.694103}} 
+          />
+          {/* {this.displayMarkers()} */}
           {/* <Marker
            onClick={this.onMarkerClick}
            name={'Gilbert, Arizona'}
           />  */}
-          <InfoWindow
+          {/* <InfoWindow
           marker={this.states.activeMarker}
           visible={this.states.showingInfoWindow}
           onClose={this.onClose}>
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow> */}
+
+        {/* <InfoWindow
+          marker={this.states.activeMarker}
+          onClose={this.onInfoWindowClose}
+          visible={this.states.showingInfoWindow}>
+          <div>
+            <h1>{this.states.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow> */}
+        {/* <div id="floating-panel">
+          <Button onclick="toggleDarkmode()">Toggle Dark Mode</Button>
+        </div> */}
+        
+        {/* <HeatMap
+          // gradient={gradient}
+          opacity = {3}
+          positions = {this.states.stores(({M, N}) => {
+          return {lat: M, lng: N};
+          })}
+          radius={30}
+        /> */}
+         {/* {this.state.isHeatmapVisible ? map : null} */}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
           <div>
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
